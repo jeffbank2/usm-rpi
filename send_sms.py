747a9f263528f251c85a6cce03941639e32f5869
jeffbank2 <bankston.jeff@gmail.com>
2026-03-17 07:43:02 -0500

    Update send_sms.py

--- send_sms.py
+++ send_sms.py
@@ -2,7 +2,7 @@ import os
 from twilio.rest import Client
 
 account_sid = os.environ["TWILIO_ACCOUNT_SID"]
-auth_token  = os.environ["TWILIO_AUTH_TOKEN"]
+auth_token = os.environ["TWILIO_AUTH_TOKEN"]
 from_number = os.environ["TWILIO_PHONE_NUMBER"]
 
 recipients = [
@@ -14,17 +14,13 @@ client = Client(account_sid, auth_token)
 try:
     with open("daily_brief.txt", "r") as f:
         brief = f.read()
-    # Keep SMS concise - just the key stats
     lines = brief.split("\n")
     sms_lines = [l for l in lines if any(x in l for x in ["RPI Rank", "Record", "Host Outlook", "Improving", "Slipping", "Flat"])]
     sms_body = "\n".join(sms_lines[:6])
 except Exception:
     sms_body = "Southern Miss RPI update complete."
 
-message = f"""Southern Miss RPI Update
-{sms_body}
-
-Dashboard: https://jeffbank2.github.io/usm-rpi/"""
+message = "Southern Miss RPI Update\n" + sms_body + "\n\nDashboard: https://jeffbank2.github.io/usm-rpi/"
 
 for number in recipients:
     client.messages.create(
@@ -32,16 +28,4 @@ for number in recipients:
         from_=from_number,
         to=number
     )
-    print(f"SMS sent to {number}")
-```
-
----
-
-## File 2: Update `requirements.txt`
-
-Edit your existing `requirements.txt` to add twilio:
-```
-requests
-beautifulsoup4
-openai
-twilio
+    print("SMS sent to " + number)
